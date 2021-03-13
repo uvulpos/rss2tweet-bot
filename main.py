be17ddda5f73840d9f798476380493bc7cc7be25
Tim Riedl <mail@tim-riedl.de>
2021-03-13 23:12:28 +0100

    imporved code quality

--- main.py
+++ main.py
@@ -5,42 +5,34 @@ from datetime import datetime, timezone
 # load .env file
 load_dotenv()
 
-# get timestamp from lst post
+#-------------------------------------------------------------------------------
+#       READ / WRITE TIMESTAMP
+#-------------------------------------------------------------------------------
+
 def get_last_post_timestamp() -> int:
 
-    # set default values
-    last_post_update = 0
     current_timestamp = int(datetime.now().timestamp())
+    last_post_update = current_timestamp
 
-    # get path to data json file
     current_dir = os.path.dirname(os.path.realpath(__file__)) + "/data.json"
 
-    # if exists -> read data, if fail -> override file
-    if os.path.isfile(current_dir):
-        try:
-            with open(current_dir, "r") as f:
-                last_post_update = int(json.loads(f.read())["last_post"])
-        except Exception as e:
-            pass
+    # read timestamp from file
+    try:
+        with open(current_dir, "r") as f:
+            last_post_update = int(json.loads(f.read())["last_post"])
+    except Exception as e:
+        pass
 
-    # update last post timestamp
+    # write timestamp to json file
     with open(current_dir, "w") as f:
         f.write(json.dumps({"last_post": current_timestamp}))
-
-    # if error or first execution set timestamp to now
-    if last_post_update == 0:
-        last_post_update = current_timestamp
 
     return last_post_update
 
 
-# fetch all articles by rss feed
-all_articles = feedparser.parse(os.getenv("rss_url"))
-last_post_timestamp = get_last_post_timestamp()
-
-#
+#-------------------------------------------------------------------------------
 #       TWITTER API AUTHENTIFICATION!
-#
+#-------------------------------------------------------------------------------
 
 # authentication of consumer key and secret
 auth = tweepy.OAuthHandler(os.getenv("twitter_consumer_key"), os.getenv("twitter_consumer_secret"))
@@ -50,9 +42,13 @@ auth.set_access_token(os.getenv("twitter_access_token"), os.getenv("twitter_acce
 api = tweepy.API(auth)
 
 
-#
+#-------------------------------------------------------------------------------
 #       loop every article
-#
+#-------------------------------------------------------------------------------
+
+# fetch all articles by rss feed
+all_articles = feedparser.parse(os.getenv("rss_url"))
+last_post_timestamp = get_last_post_timestamp()
 
 for article in all_articles["entries"]:
 
